4aedbd03d6368adc3891c38b35e6841dc01620cc
Axelfqt <axel.fouquet@3wa.io>
2023-05-17 16:52:49 +0200

    routes

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -5,6 +5,8 @@ import Create from '../views/Article/Create.vue';
 import Update from '../views/Article/Update.vue';
 import Login from '../views/Auth/Login.vue';
 import Register from '../views/Auth/Register.vue';
+import Search from '../views/User/Search.vue';
+import Profil from '../views/User/Profil.vue';
 
 const routes = [
     {
@@ -36,6 +38,16 @@ const routes = [
         path: '/register',
         name: 'register',
         component: Register
+    },
+    {
+        path: '/users',
+        name: 'users',
+        component: Search
+    },
+    {
+        path: '/user/:name',
+        name: 'user',
+        component: Profil
     },
 ]
 

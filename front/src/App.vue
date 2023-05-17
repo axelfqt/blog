c786279635ec191323d8210e3d00d3d6d291fd33
Axelfqt <axel.fouquet@3wa.io>
2023-05-17 16:52:38 +0200

    add search

--- front/src/App.vue
+++ front/src/App.vue
@@ -16,14 +16,14 @@
             <li><a class="hover:text-gray-200" href="/profil">Mon profil</a></li>
             <li><a class="hover:text-gray-200" @click="userLogout">Déconnexion</a></li>
           </ul>
+          <a href="/users"><svg class="text-white-600 h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
+            viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve"
+            width="512px" height="512px">
+            <path
+              d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
+          </svg></a>
         </div>
-        <a class="xl:hidden flex mr-6 items-center" href="#">
-          <span class="flex absolute -mt-5 ml-4">
-            <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-            <span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
-            </span>
-          </span>
-        </a>
         <a class="navbar-burger self-center mr-12 xl:hidden" href="#"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" /></svg></a>
       </nav>
     </section>
@@ -36,10 +36,9 @@
   import useAuth from "./resources/js/composables/Auth";
 
   const {isLogin, getUserToken, logout} = useAuth();
+
   const userLogout = async () => {
     await logout()
   }
   onMounted(() => getUserToken());
-
-  console.log(isLogin);
 </script>
